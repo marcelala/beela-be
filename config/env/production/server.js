d341017156a51cac52bb3438bd72e1dbb8bd1d16
Marcela Felix Fortis <marmarmarcelacela@gmail.com>
2022-03-12 20:19:39 +0100

    fix heroku server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  url: process.env('DATABASE_URL'),
+  url: env('DATABASE_URL'),
   proxy: true,
-  port: process.env.int('PORT', 1337),
+  port: env.int('PORT', 1337),
   app: {
-    keys: process.env.array('APP_KEYS', ['KEY1', 'KEY2','KEY3','KEY4']),
+    keys: env.array('APP_KEYS', ['KEY1', 'KEY2','KEY3','KEY4']),
   },
 });
