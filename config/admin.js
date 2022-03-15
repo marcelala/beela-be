debecaa5780be29345c9fff4c1736445875ffc20
Marcela Felix Fortis <marmarmarcelacela@gmail.com>
2022-03-15 22:36:05 +0100

    heroku configuration

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', '434800139b80dddb80872f324161cc3f'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('LOCAL_HOST','0.0.0.0'),
-  proxy: true,
   port: env.int('NODE_PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['KEY1', 'KEY2','KEY3','KEY4']),
