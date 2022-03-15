008ad6dba019eb63fd4cb8895dd22af811b7cdfe
Marcela Felix Fortis <marmarmarcelacela@gmail.com>
2022-03-15 22:59:43 +0100

    heroku configuration zz

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  url: env('HEROKU_URL'),
-  host:env('HOST'),
-  proxy: true,
+  host:env('LOCAL_HOST'),
   port: env.int('NODE_PORT', 1337),
+  url: env('HEROKU_URL'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS', ['KEY1', 'KEY2','KEY3','KEY4']),
   },
