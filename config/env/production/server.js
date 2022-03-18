0a1a80ae60f600b9a2572005cb39a0ba56f666f9
Marcela Felix Fortis <marmarmarcelacela@gmail.com>
2022-03-18 22:23:36 +0100

    heroku config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host:env('HOST','0.0.0.0'),
-  port: env.int('NODE_PORT', 1337),
+  host: "0.0.0.0",
   url: env('HEROKU_URL'),
-  proxy: true,
   app: {
     keys: env.array('APP_KEYS', ['KEY1', 'KEY2']),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
